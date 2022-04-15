fcd33708a5aadcfab0d3e5dd106f62647b3fe878
Rui Vilela <ruivil@gmail.com>
2022-04-15 16:02:41 +0100

    Work state

--- RandomPartExplosion/RPExplode.cs
+++ RandomPartExplosion/RPExplode.cs
@@ -12,14 +12,31 @@ namespace RandomPartExplosion
     {
         public void Startup()
         {
-            bool key = Input.GetKey(KeyCode.RightAlt) && Input.GetKey(KeyCode.Alpha0);
-            if (!key) return;
-
-            List<Part> parts = FlightGlobals.ActiveVessel.Parts;
-            int index;
-            System.Random rnd = new System.Random();
-            index = rnd.Next(1, parts.Count);
-            parts[index].explode();
+            Debug.Log("[RPE] Start!");
+        }
+        public void Update()
+        {
+            bool keyPressed = Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.Alpha4);
+            if (keyPressed)
+            {
+                List<Part> parts = FlightGlobals.ActiveVessel.Parts;
+                int index;
+                System.Random rnd = new System.Random();
+                Debug.Log($"[RPE] Kaboom, part count: {parts.Count}");
+                index = rnd.Next(1, parts.Count);
+                parts[index].explode();
+            }
+            
+            keyPressed = Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.Alpha5);
+            if (keyPressed)
+            {
+                Debug.Log("[RPE] SelfDestruct");
+                List<Part> parts = FlightGlobals.ActiveVessel.Parts;
+                for (int x = parts.Count - 1; x >= 0; x--) // This actually looks like a single explosion
+                {
+                    parts[x].explode();
+                }
+            }
         }
     }
 }
